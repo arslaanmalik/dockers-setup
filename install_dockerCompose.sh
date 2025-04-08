8ef0608e3d984c71d623cc9da9641e3e8ae4b9d4
Arslaan Ahmad Malik <arslaanmalik@yahoo.com>
2025-04-08 14:16:47 +0300

    updated docker compose

--- install_dockerCompose.sh
+++ install_dockerCompose.sh
@@ -1,7 +1,6 @@
-sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-
+sudo curl -L https://github.com/docker/compose/releases/download/2.34.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 echo 'Installed Docker Compose Now Giving Root Permisions'
 
 sudo chmod +x /usr/local/bin/docker-compose
 
-docker-compose -v
+docker compose version
